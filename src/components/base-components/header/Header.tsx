836a22d8cfdeabc220e90474b85692b539e337ca
Maximushka <m.billsrc@gmail.com>
2021-04-24 00:07:30 +0700

    Добавлено меню-бургер

--- src/components/base-components/header/Header.tsx
+++ src/components/base-components/header/Header.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import s from './Header.module.scss'
 import NavBar from './navBar/NavBar';
 import styleContainer from '../../../common/styles/Container.module.css'
+import BurgerNavBar from './burgerNavBar/BurgerNavBar';
 
 const Header: React.FC = () => {
+    const blockNames = [{name: 'Главная', link: 'main'}, {name: 'Обо мне', link: 'aboutMe'},
+        {name: 'Навыки', link: 'skills'}, {name: 'Проекты', link: 'projects'}, {name: 'Контакты', link: 'contacts'},]
+
     return (
         <div className={s.header}>
             <div className={`${styleContainer.container} ${s.headerContainer}`}>
-                <NavBar />
+                <NavBar blockNames={blockNames} />
+                <BurgerNavBar blockNames={blockNames} />
             </div>
         </div>
     );
---
+++ src/components/base-components/header/burgerNavBar/BurgerNavBar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import s from './BurgerNavBar.module.scss'
+import {BlockNamesType} from '../navBar/NavBar';
+import {Link} from 'react-scroll';
+import { useState } from 'react';
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
+const BurgerNavBar: React.FC<BlockNamesType> = ({blockNames}) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const burgerMenuItemsStyle = isOpen ? {} : {transform: 'translate(0, -250px)'}
+
+    return (
+        <div className={s.burgerMenu}>
+            <div style={burgerMenuItemsStyle} className={s.burgerMenuItems}>
+                {
+                    blockNames.map(el => {
+                        return <Link
+                            onClick={() => setIsOpen(false)}
+                            activeClass={s.active}
+                            to={el.link}
+                            spy={true}
+                            smooth={true}
+                            offset={-100}
+                            duration={500}
+                        >{el.name}</Link>
+                    })
+                }
+            </div>
+            <div onClick={() => setIsOpen(!isOpen)} className={s.burgerMenuButton}>
+                <FontAwesomeIcon className={s.icon}  icon={faBars} />
+            </div>
+        </div>
+    );
+};
+
+export default BurgerNavBar;
